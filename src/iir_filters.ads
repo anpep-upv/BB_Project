7f28b0dfe5fcb8cc7a182c216e059c3f93d22288
anpep <anpep@outlook.com>
2022-01-17 15:57:20 +0100

    iir_filters: add dummy filter to package spec

--- src/iir_filters.ads
+++ src/iir_filters.ads
@@ -4,4 +4,5 @@ with BB.ADC; use BB.ADC;
 package IIR_Filters is
    function Filter_80 (ADC_Sample : in ADC_Register) return Position;
    function Filter_50 (ADC_Sample : in ADC_Register) return Position;
+   function Filter_Dummy (ADC_Sample : in ADC_Register) return Position;
 end IIR_Filters;
